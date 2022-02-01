66b5804dfeb17fb25e7f3a68d1536fae9bd4a6e3
Cinta Ramayanti <cinta.ramayanti@gmail.com>
2022-02-01 15:27:59 +0700

    fix export

--- app/Exports/ComplainExport.php
+++ app/Exports/ComplainExport.php
@@ -6,25 +6,33 @@ use App\Models\Complain;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class ComplainExport implements FromCollection, WithHeadings, ShouldAutoSize
+class ComplainExport implements FromCollection, WithHeadings, ShouldAutoSize, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Complain::all();
+        return Complain::with('user')->get();
+    }
+
+    public function map($complain) : array {
+        return [
+            $complain->id,
+            $complain->user->name,
+            strip_tags($complain->pesan_complain),
+        ] ;
     }
 
     public function headings(): array
     {
         return [
             'id',
-            'user_id',
+            'nama user',
             'pesan_complain',
-            'created_at',
-            'updated_at'
+
         ];
     }
 }
--- app/Exports/RenovasiExport.php
+++ app/Exports/RenovasiExport.php
@@ -6,29 +6,40 @@ use App\Models\Renovasi;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class RenovasiExport implements FromCollection, WithHeadings, ShouldAutoSize
+class RenovasiExport implements FromCollection, WithHeadings, ShouldAutoSize, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Renovasi::all();
+        return Renovasi::with('user', 'nomer')->get();
+    }
+
+    public function map($renovasi) : array {
+        return [
+            $renovasi->id,
+            $renovasi->user->name,
+            $renovasi->nomer->no_rumah,
+            $renovasi->tanggal_mulai,
+            $renovasi->tanggal_akhir,
+            strip_tags($renovasi->catatan_renovasi),
+            strip_tags($renovasi->catatan_biasa),
+        ] ;
     }
 
     public function headings(): array
     {
         return [
             'id',
-            'user_id',
-            'rumah_id',
+            'nama user',
+            'no rumah',
             'tanggal_mulai',
             'tanggal_akhir',
             'catatan_renovasi',
-            'catatan_biasa',
-            'created_at',
-            'updated_at'
+
         ];
     }
 }
